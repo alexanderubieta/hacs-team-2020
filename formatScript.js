cba96482cbce1b53d50c456baca8bdf8c7a46653
BlazeATG <ubietaalex@gmail.com>
2020-10-25 09:50:46 -0500

    Polished Look

---
+++ formatScript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    document.getElementById('center').scrollIntoView({ inline: 'center' });
+    document.getElementById('center1').scrollIntoView({ inline: 'center' });
+    document.getElementById('center2').scrollIntoView({ inline: 'center' });
+    document.getElementById('center3').scrollIntoView({ inline: 'center' });
+    document.getElementById('center4').scrollIntoView({ inline: 'center' });
+    document.getElementById('center5').scrollIntoView({ inline: 'center' });
+    document.getElementById('center6').scrollIntoView({ inline: 'center' });
+    document.getElementById('center7').scrollIntoView({ inline: 'center' });
+    document.getElementById('center8').scrollIntoView({ inline: 'center' });
+    document.getElementById('center9').scrollIntoView({ inline: 'center' });
+});
